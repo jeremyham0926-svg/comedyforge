c810aa8aefa30f854449c9d28f517a12b36b062c
jeremyham0926-svg <jeremyham0926@gmail.com>
2026-07-07 14:42:15 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,15 +5,17 @@ fetch('https://comedyforge.onrender.com/api/characters')
     const container = document.getElementById('characters');
 
     characters.forEach(char => {
-      const div = document.createElement('div');
-      div.className = 'character-card';
+  const div = document.createElement('div');
+  div.className = 'character-card';
 
-      div.innerHTML = `
-        <div class="character-name">${char.name}</div>
-        <div class="character-description">${char.description}</div>
-      `;
+  div.innerHTML = `
+    <img class="character-image" src="images/${char.image}" alt="${char.name}">
+    <div class="character-name">${char.name}</div>
+    <div class="character-description">${char.description}</div>
+  `;
 
-      container.appendChild(div);
-    });
+  container.appendChild(div);
+});
+
   })
   .catch(err => console.error('Error fetching characters:', err));

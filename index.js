655881a3ff324e42b408d963b00fc5cbff6e1473
jeremyham0926-svg <jeremyham0926@gmail.com>
2026-07-08 16:24:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,17 +6,24 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Serve index.html from the root
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
-app.use('/images', express.static(__dirname + '/images'));
 
-app.use(express.static(__dirname));
+// Serve static files from /public
+app.use(express.static('public'));
 
+// Serve images from /public/images
+app.use('/images', express.static(path.join(__dirname, 'public/images')));
+
+// API routes
 const characterRoutes = require('./routes/characters');
 app.use('/api/characters', characterRoutes);
 
+// Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
+

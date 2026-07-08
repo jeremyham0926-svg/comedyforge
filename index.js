4e9478f37e685cec000b0e6b8e465a2ae868c22c
jeremyham0926-svg <jeremyham0926@gmail.com>
2026-07-08 09:42:25 -0400

    Update and rename old-index.js to index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.use(express.static(__dirname));
+
+const characterRoutes = require('./routes/characters');
+app.use('/api/characters', characterRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
